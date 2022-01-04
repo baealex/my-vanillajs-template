4f9e95707201c82387a3b1ce7394a5ac7ffeb711
Jino Bae <im@baejino.com>
2022-01-04 14:07:41 +0900

    🔨 Change config for typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: "./src/index.ts",
   module: {
     rules: [
       {
-        test: /\.(ts|js)?$/,
+        test: /\.(ts|tsx)?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-typescript"],
-          },
         },
-      },
+      }
     ],
   },
   resolve: {
     extensions: [".ts", ".js"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html"
+    }),
+  ],
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: true,
   },
   devServer: {
-    static: path.join(__dirname, "public"),
+    static: path.join(__dirname, "dist"),
     allowedHosts: 'all',
-    compress: true,
   },
 };
